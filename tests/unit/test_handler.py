91e7b9e0ca4dc9febf81da44bc9db21af7f0bd24
Sergey Orlov <serhio.orlovsky@gmail.com>
2019-06-01 14:09:13 +0700

    clean up test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,8 +1,6 @@
-import json
-
 import pytest
 
-from hello_world import app
+from telegram_bot import app
 
 
 @pytest.fixture()
@@ -63,11 +61,5 @@ def apigw_event():
 
 
 def test_lambda_handler(apigw_event, mocker):
-
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
-
+    ret = app.webhook(apigw_event, "")
     assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
